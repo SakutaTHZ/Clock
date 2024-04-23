ea97d9bfd9fb572076765d86c03e0b498a3e2de5
Sakuta <110976118+SakutaTHZ@users.noreply.github.com>
2024-04-23 16:53:14 +0630

    updated

--- main.js
+++ main.js
@@ -91,16 +91,29 @@ let clockFace = {
     }
 }
 
-
+let second2 = 0
+let second1 = 0
+let minute2 = 0
+let minute1 = 0
 const counter = ()=>{
     setInterval(()=>{
-        const inputValue = Math.floor(Math.random() * 10);
+        second2++
+        const inputValue = second2%10;
     
         if (clockFace.hasOwnProperty(inputValue)) {
             const selectedObject = clockFace[inputValue];
             
             setTimeout(() => {
-                setData(selectedObject)
+                setData("Second2",selectedObject)
+                if(inputValue == 0){
+                    second1++
+                    if(second1 == 6){
+                        second1 = 0
+                        setData("Second1",clockFace[second1])
+                    }else{
+                        setData("Second1",clockFace[second1])
+                    }
+                }
             }, 100);
         } else {
             console.log(`Object with key "${inputValue}" not found`);
@@ -108,14 +121,14 @@ const counter = ()=>{
     }, 1000)
 }
 
-const setData = (data)=>{
-    data.handc?document.querySelector('.handc').classList.add('on'):document.querySelector('.handc').classList.remove('on')
-    data.hand1?document.querySelector('.hand1').classList.add('on'):document.querySelector('.hand1').classList.remove('on')
-    data.hand2?document.querySelector('.hand2').classList.add('on'):document.querySelector('.hand2').classList.remove('on')
-    data.hand3?document.querySelector('.hand3').classList.add('on'):document.querySelector('.hand3').classList.remove('on')
-    data.hand4?document.querySelector('.hand4').classList.add('on'):document.querySelector('.hand4').classList.remove('on')
-    data.hand5?document.querySelector('.hand5').classList.add('on'):document.querySelector('.hand5').classList.remove('on')
-    data.hand6?document.querySelector('.hand6').classList.add('on'):document.querySelector('.hand6').classList.remove('on')
+const setData = (boxname,data)=>{
+    data.handc?document.querySelector(`.${boxname}>.handc`).classList.add('on'):document.querySelector(`.${boxname}>.handc`).classList.remove('on')
+    data.hand1?document.querySelector(`.${boxname}>.hand1`).classList.add('on'):document.querySelector(`.${boxname}>.hand1`).classList.remove('on')
+    data.hand2?document.querySelector(`.${boxname}>.hand2`).classList.add('on'):document.querySelector(`.${boxname}>.hand2`).classList.remove('on')
+    data.hand3?document.querySelector(`.${boxname}>.hand3`).classList.add('on'):document.querySelector(`.${boxname}>.hand3`).classList.remove('on')
+    data.hand4?document.querySelector(`.${boxname}>.hand4`).classList.add('on'):document.querySelector(`.${boxname}>.hand4`).classList.remove('on')
+    data.hand5?document.querySelector(`.${boxname}>.hand5`).classList.add('on'):document.querySelector(`.${boxname}>.hand5`).classList.remove('on')
+    data.hand6?document.querySelector(`.${boxname}>.hand6`).classList.add('on'):document.querySelector(`.${boxname}>.hand6`).classList.remove('on')
     
 }
 
